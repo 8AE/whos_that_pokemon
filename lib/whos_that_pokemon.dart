6f680a919c94b9860031cfd0d0f157f5c54161c8
8ae <aelbaba33@outlook.com>
2024-05-27 22:32:14 -0400

    more progress

--- lib/whos_that_pokemon.dart
+++ lib/whos_that_pokemon.dart
@@ -6,66 +6,58 @@ import 'dart:math';
 import 'dart:convert';
 
 class WhosThatPokemon extends StatefulWidget {
-  const WhosThatPokemon({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const WhosThatPokemon({super.key});
 
   @override
   State<WhosThatPokemon> createState() => _WhosThatPokemonMainState();
 }
 
 class _WhosThatPokemonMainState extends State<WhosThatPokemon> {
-  String _pokemonName = '';
-  String _pokemonType1 = 'single_type';
-  String _pokemonType2 = 'single_type';
-  String _pokemonImageUrl = 'https://picsum.photos/250?image=9';
-
   Future<http.Response> _getRandomPokemonRaw() {
-    var intValue = Random().nextInt(1200) + 1;
+    var intValue = Random().nextInt(150) + 1; // only OG for now
 
     return http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/$intValue'));
   }
 
-  Future<void> _generatePokemon() async {
+  Future<Pokemon> _generatePokemon() async {
     var data = await _getRandomPokemonRaw();
     Pokemon randomPokemon = Pokemon.fromHttpBody(data.body);
 
-    setState(() {
-      _pokemonName = randomPokemon.name;
-      _pokemonType1 = randomPokemon.type1;
-      _pokemonType2 = randomPokemon.type2;
-      _pokemonImageUrl = randomPokemon.spriteImageUrl;
-    });
+    return randomPokemon;
+  }
+
+  Future<void> _refreshPokemon() async {
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            "Who's that pokemon?",
-            style: Theme.of(context).textTheme.headlineLarge,
-          ),
-          Text(
-            _pokemonName,
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-          PokemonType(_pokemonType1, _pokemonType2),
-          Image.network(_pokemonImageUrl),
-          TextButton(onPressed: _generatePokemon, child: const Text("Generate New Pokemon"))
-        ],
-      ),
-    );
+        child: FutureBuilder<Pokemon>(
+            future: _generatePokemon(),
+            builder: (BuildContext context, AsyncSnapshot<Pokemon> snapshot) {
+              List<Widget> children;
+              if (snapshot.hasData) {
+                children = [
+                  Text(
+                    "Who's that pokemon?",
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  ),
+                  Text(
+                    snapshot.data!.name,
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  PokemonType(snapshot.data!.type1, snapshot.data!.type2),
+                  Image.network(snapshot.data!.spriteImageUrl),
+                  TextButton(onPressed: _refreshPokemon, child: const Text("Generate New Pokemon"))
+                ];
+              } else {
+                children = [const CircularProgressIndicator()];
+              }
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: children,
+              );
+            }));
   }
 }
