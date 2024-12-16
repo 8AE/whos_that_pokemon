e7bf003c33b192091ed76cbeb25504897b8ebf99
8ae <aelbaba33@outlook.com>
2024-12-16 15:24:54 -0500

    idk

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,12 @@
+# Check if Flutter is installed
+if ! command -v flutter &> /dev/null
+then
+    echo "Flutter is not installed. Installing Flutter..."
+    git clone https://github.com/flutter/flutter.git -b stable
+    export PATH="$PATH:`pwd`/flutter/bin"
+else
+    echo "Flutter is already installed."
+fi
+
+# Proceed with the deployment
 make deploy OUTPUT=whos_that_pokemon_client
